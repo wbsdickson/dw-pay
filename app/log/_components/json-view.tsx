4bacfc992288f2c9c46810dc7389b95e5189cccd
Dickson Wu <wbs.dickson@gmail.com>
2023-10-08 09:09:55 -0600

    fix ui log

--- app/log/_components/json-view.tsx
+++ app/log/_components/json-view.tsx
@@ -5,7 +5,7 @@ export default function JSONView({ data }: any) {
     return (
         <>
             <h1>DW Pay transaction log</h1>
-            {data && data.data.length > 0 && (
+            {data.data && data.data.length > 0 && (
                 <>
                     <h2>Total Transaction : {data.data.length}</h2>
                     <JsonView data={data} />
