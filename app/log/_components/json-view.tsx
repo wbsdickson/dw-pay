28bf52373531184ce66277dcfb1c4e40a38fe25b
Dickson Wu <wbs.dickson@gmail.com>
2023-10-08 09:04:17 -0600

    update UI

--- app/log/_components/json-view.tsx
+++ app/log/_components/json-view.tsx
@@ -2,5 +2,11 @@
 import { JsonView } from "react-json-view-lite";
 import "react-json-view-lite/dist/index.css";
 export default function JSONView({ data }: any) {
-    return <JsonView data={data} />;
+    return (
+        <>
+            <h1>DW Pay transaction log</h1>
+            <h2>Total Transaction : {data.data.length}</h2>
+            <JsonView data={data} />
+        </>
+    );
 }
